79520caccc752de63dcc5617304f53086d6e82b2
Marlon Enrike <kurtyebsb@gmail.com>
2025-10-25 03:39:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7378602289-6f285",
+  "appId": "1:944684600941:web:406a2b84488f68d1a6f964",
+  "apiKey": "AIzaSyBOjCNr5QpyK-1fv2lgMBV-P-cqkjsNbCE",
+  "authDomain": "studio-7378602289-6f285.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "944684600941"
+};
