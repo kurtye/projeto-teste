2ff2ab6e7391357ce101404dbc94d97aa379d50e
Marlon Enrike <kurtyebsb@gmail.com>
2025-10-27 00:24:30 +0000

    antes de publicar , quero fazer o seguinte ajuste, caso mobile ocultar o

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -8,11 +8,11 @@ export function Header() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/80 backdrop-blur-sm">
       <div className="container flex h-16 items-center justify-between space-x-4">
-        <div className="flex gap-2 md:gap-10 items-center">
+        <div className="flex gap-2 md:gap-4 items-center">
           <SidebarTrigger className="md:hidden" />
           <Link href="/" className="flex items-center space-x-2">
             <ArmyHelmetIcon className="h-8 w-8 text-primary" />
-            <span className="hidden md:inline-block font-bold font-headline text-lg">
+            <span className="font-bold font-headline text-lg">
               Hell Let Loose BR
             </span>
           </Link>
