f2952249f170c90c07baf2c5897dade2c22c6fc0
Marlon Enrike <kurtyebsb@gmail.com>
2025-10-27 01:00:24 +0000

    aqui esta o measurementId: "G-5P5ELS9WSX"

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,6 @@ export const firebaseConfig = {
   "appId": "1:944684600941:web:406a2b84488f68d1a6f964",
   "apiKey": "AIzaSyBOjCNr5QpyK-1fv2lgMBV-P-cqkjsNbCE",
   "authDomain": "studio-7378602289-6f285.firebaseapp.com",
-  "measurementId": "",
+  "measurementId": "G-5P5ELS9WSX",
   "messagingSenderId": "944684600941"
 };
