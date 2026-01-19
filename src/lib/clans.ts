8d1e968d2c1e1021693dfedcaffbd03a7831bf50
Marlon Enrike <kurtyebsb@gmail.com>
2026-01-19 16:38:42 +0000

    em clas da comunidade coloque SMK como "Smoking Snakes", a HRB como "Hel

--- src/lib/clans.ts
+++ src/lib/clans.ts
@@ -2,11 +2,11 @@
 import type { Clan } from './types';
 
 export const clans: Clan[] = [
-    { id: 'smk', name: 'Senta a Pua - Smoke Cobras', tag: 'SMK', adminEmails: ['smk@test.com'] },
-    { id: 'hrb', name: 'Hell Regnum Brasil', tag: 'HRB', adminEmails: ['hrb@test.com'] },
-    { id: 'rzn', name: 'Razantes', tag: 'RZN', adminEmails: ['rzn@test.com'] },
-    { id: 'ocl', name: 'Os Cumpadi di Lampião', tag: 'OCL', adminEmails: ['ocl@test.com'] },
-    { id: '3lpz', name: '3. Leichte Panzerdivision', tag: '3LPZ', adminEmails: ['3lpz@test.com'] },
+    { id: 'smk', name: 'Smoking Snakes', tag: 'SMK', adminEmails: ['smk@test.com'] },
+    { id: 'hrb', name: 'Hellraiser Brasil', tag: 'HRB', adminEmails: ['hrb@test.com'] },
+    { id: 'rzn', name: 'RedZone Brasil', tag: 'RZN', adminEmails: ['rzn@test.com'] },
+    { id: 'ocl', name: 'O Caba Lá', tag: 'OCL', adminEmails: ['ocl@test.com'] },
+    { id: '3lpz', name: 'LAPA AZUL', tag: '3LPZ', adminEmails: ['3lpz@test.com'] },
     { id: 'wrt', name: 'War Tuga', tag: 'WRT', adminEmails: ['wrt@test.com'] },
     { id: 'sap', name: 'Senta a Púa', tag: 'SAP', adminEmails: ['sap@test.com'] },
     { id: 'bold', name: 'B.O.L.D', tag: 'BOLD', adminEmails: ['bold@test.com'] },
