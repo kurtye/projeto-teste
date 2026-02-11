8acd9f1120e71cac1ea7b6ef8e40ee4532ebc6e6
Marlon Enrike <kurtyebsb@gmail.com>
2026-02-11 22:41:13 +0000

    add logo clans

--- src/lib/clans.ts
+++ src/lib/clans.ts
@@ -4,12 +4,11 @@ import type { Clan } from './types';
 export const clans: Clan[] = [
     { id: 'smk', name: 'Smoking Snakes', tag: 'SMK', adminEmails: ['smk@test.com'], logoUrl: '/clan/smk.jpg' },
     { id: 'hrb', name: 'Hellraiser Brasil', tag: 'HRB', adminEmails: ['admin@hrb.com'], logoUrl: '/clan/hrb.jpg' },
-    { id: 'rzn', name: 'RedZone Brasil', tag: 'RZN', adminEmails: ['rzn@test.com'] },
-    { id: 'ocl', name: 'O Caba Lá', tag: 'OCL', adminEmails: ['ocl@test.com'] },
-    { id: '3lpz', name: 'LAPA AZUL', tag: '3LPZ', adminEmails: ['3lpz@test.com'] },
-    { id: 'wrt', name: 'War Tuga', tag: 'WRT', adminEmails: ['wrt@test.com'] },
-    { id: 'sap', name: 'Senta a Púa', tag: 'SAP', adminEmails: ['sap@test.com'] },
+    { id: 'rzn', name: 'RedZone Brasil', tag: 'RZN', adminEmails: ['rzn@test.com'],  logoUrl: '/clan/rzn.jpg' },
+    { id: 'ocl', name: 'O Caba Lá', tag: 'OCL', adminEmails: ['ocl@test.com'],  logoUrl: '/clan/ocl.jpg' },
+    { id: '3lpz', name: '3 Lapa Azul', tag: '3LPZ', adminEmails: ['3lpz@test.com'],  logoUrl: '/clan/3lpz.jpg' },
+    { id: 'wrt', name: 'War Regimente', tag: 'WRT', adminEmails: ['wrt@test.com'],  logoUrl: '/clan/wrt.jpg' },
+    { id: 'sap', name: 'Senta a Púa', tag: 'SAP', adminEmails: ['sap@test.com'],  logoUrl: '/clan/sap.jpg' },
     { id: 'bold', name: 'B.O.L.D', tag: 'BOLD', adminEmails: ['bold@test.com'] },
-    { id: 'idg', name: 'Irmandade de Guerra', tag: 'IDG', adminEmails: ['idg@test.com'] },
-    { id: 'soh', name: 'Sons of Hell', tag: 'SOH', adminEmails: ['soh@test.com'] },
+    { id: 'soh', name: 'Sons of Hell', tag: 'SOH', adminEmails: ['soh@test.com'],  logoUrl: '/clan/soh.jpg' },
 ];
